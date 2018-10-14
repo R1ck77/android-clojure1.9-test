f3ab53b8a4a3a10de902a5810adf74fa814b9bad
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-10-14 18:27:05 +0200

    Test with uberjar and clojure 1.8. Works on my Android 8

--- app/src/main/java/couchgames/clojure19test/MainActivity.java
+++ app/src/main/java/couchgames/clojure19test/MainActivity.java
@@ -2,6 +2,10 @@ package couchgames.clojure19test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import clojure.java.api.Clojure;
+import test.library.ClojureLibrary;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        System.out.println("MainActivity.onStart");
+        TextView textView = (TextView) findViewById(R.id.output);
+        System.out.println("Clojure code executing...");
+        textView.setText(ClojureLibrary.sayHello());
+        System.out.println("...executed");
     }
 }
